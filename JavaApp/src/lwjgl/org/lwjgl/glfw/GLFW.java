08b21e5ff914dac8cb1e97fb4226c0cfe84c72b4
kutta <taylenwells2026@icloud.com>
2026-07-05 17:58:42 -0600

    Development Build
    Signed-off-by: kutta <taylenwells2026@icloud.com>

--- JavaApp/src/lwjgl/org/lwjgl/glfw/GLFW.java
+++ JavaApp/src/lwjgl/org/lwjgl/glfw/GLFW.java
@@ -519,8 +519,7 @@ public class GLFW
     public static final ByteBuffer keyDownBuffer = ByteBuffer.allocateDirect(317);
     public static long mainContext = 0;
 
-    private static native void
-static {
+    static {
         try {
             System.load(System.getenv("BUNDLE_PATH") + "/AngelAuraAmethyst");
 } catch (UnsatisfiedLinkError e) {
